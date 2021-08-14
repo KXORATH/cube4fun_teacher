426391ceda1020443133f04443f2d3ff5dc4ebe0
KXORATH <minecraftKXORATH@gmail.com>
2021-08-14 18:26:06 +0200

    Dodanie zmian

---
+++ docs/js/main.js
@@ -0,0 +1,8 @@
+function copyToClipboard(element) {
+    var $temp = $("<textarea>");
+    var brRegex = /<br\s*[\/]?>/gi;
+    $("body").append($temp);
+    $temp.val($(element).html().replace(brRegex, "\r\n")).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
