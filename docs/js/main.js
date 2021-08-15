c9dde4be9cfbe477d96b3502ad68821554f762ed
KXORATH <minecraftKXORATH@gmail.com>
2021-08-15 18:46:45 +0200

    Dodanie nava i darkmoda

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,3 +6,33 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
 }
+
+function imageToBlob(imageURL) {
+    const img = new Image;
+    const c = document.createElement("canvas");
+    const ctx = c.getContext("2d");
+    img.crossOrigin = "";
+    img.src = imageURL;
+    return new Promise(resolve => {
+        img.onload = function () {
+            c.width = this.naturalWidth;
+            c.height = this.naturalHeight;
+            ctx.drawImage(this, 0, 0);
+            c.toBlob((blob) => {
+                // here the image is a blob
+                resolve(blob)
+            }, "image/png", 0.75);
+        };
+    })
+}
+
+async function copyImage(imageURL){
+    const blob = await imageToBlob(imageURL)
+    const item = new ClipboardItem({ "image/png": blob });
+    navigator.clipboard.write([item]);
+}
+
+function darkMode() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+}
